8124fe04ea87921fef7483245b736f77f8139a9c
John Doe <johndoe@email.com>
2025-03-04 21:16:29 -0500

    cambios con actividad

--- Pagina Web/index.js
+++ Pagina Web/index.js
@@ -2,3 +2,15 @@ let mensajeNuevo="El mensaje ha sido actualizado satisfactoriamente"
 function cambiarMensaje() {
     document.getElementById("mensaje").innerText = mensajeNuevo;
 }
+
+$(document).ready(function() {
+    $('#btn-cambiar').click(function() {
+        $('.tarjeta').css('background', 'radial-gradient(circle, rgba(0,255,255,1) 0%, rgba(255,0,255,1) 100%)');
+
+        $('.nombre p').css('color', 'white'); 
+        $('.titulo p').css('color', 'white'); 
+        $('.descripcion p').css('color', 'white');
+
+        alert("¡Todo ha cambiado!");
+    });
+});
